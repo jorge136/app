f1e93262e21e31af3b921b0a3997008e92f03cb5
Jorge <3126364249z@gmail.com>
2024-02-01 23:41:49 -0500

    sextocommit

--- Documents/app/appTickets/src/app/register/register.page.ts
+++ Documents/app/appTickets/src/app/register/register.page.ts
@@ -2,22 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  registerForm: FormGroup; // declarar la propiedad registerForm
-  // mensajes de validación y respuesta de registro
-  registerMessage: string = ''; // agregar variable para mensajes de registro
-
+  registerForm: FormGroup; validation_messages = {
+    email: [
+      { type: "required", message: "El Email es obligatorio." },
+      { type: "pattern", message: "El Email ingresado no es valido." }
+    ],
+    password: [
+      {type: "required", message: "La Contraseña es obligatoria."},
+      {type: "minlength", message: "La contraseña debe contener minimo 8 caracteres."}
+    ],
+    confirmation_password: [
+      {type: "required", message: "Las contraseñas no cinciden."},
+      {type: "minlength", message: "Las contraseñas no coinciden."}
+    ],
+    name: [
+      {type: "required", message: "El nombre es obligatorio."},
+      {type: "maxlength", message: "El maximo de de caracteres es de 20."}
+    ],
+    last_name: [
+      {type: "required", message: "El apellido es obligatorio."},
+      {type: "maxlength", message: "El maximo de de caracteres es de 20."}
+    ]
+  }
+  registerMessage: any;
   constructor(
+    private router: Router,
     private formBuilder: FormBuilder,
     private navCtrl: NavController,
     private storage: Storage
-  ) { 
+  ) {
+   
     this.registerForm = this.formBuilder.group({
       email: new FormControl(
         "",
@@ -28,49 +49,49 @@ export class RegisterPage implements OnInit {
           )
         ])
       ),
+     
       password: new FormControl(
         "",
         Validators.compose([
           Validators.required,
-          // Puedes agregar más validadores según tus requisitos
+          Validators.minLength(8)
         ])
       ),
       confirmation_password: new FormControl(
         "",
         Validators.compose([
           Validators.required,
-          // Puedes agregar más validadores según tus requisitos
+          Validators.minLength(8)
         ])
       ),
       name: new FormControl(
         "",
         Validators.compose([
           Validators.required,
-          // Puedes agregar más validadores según tus requisitos
+          Validators.maxLength(20)
         ])
       ),
       last_name: new FormControl(
         "",
         Validators.compose([
-          // Puedes agregar más validadores según tus requisitos
+          Validators.required,
+          Validators.maxLength(20)
         ])
       )
-    });
+    })
+    }
+    goToHome(){
+      console.log("go to home");
+      this.router.navigateByUrl('/menu/home');
+      this.storage.set('mostreLaIntro', true);
+    }
+  ngOnInit() {
   }
 
-  ngOnInit() {}
-
-  register(register_data: any) {
+  register(register_data: any){
     console.log(register_data);
-
-    // Agregar lógica de registro aquí
-    // Por ejemplo, podrías llamar a un servicio de registro
-
-    // Simulando un mensaje de éxito para propósitos de demostración
-    this.registerMessage = '¡Registro exitoso!';
+      this.storage.set('userRegisteredIn', true);
+      this.navCtrl.navigateForward('/menu/home');
+    }; 
   }
 
-  goToLogin() {
-    this.navCtrl.navigateBack('/login');
-  }
-}
